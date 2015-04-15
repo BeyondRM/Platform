20fe5bfb478b936b60ca792972a4f7923401a2a9
JustGregory <gregory.cheyney@gmail.com>
2015-04-14 20:51:46 -0500

    Adding dependency; correcting superclass.
    As described.

--- platform-species/src/main/java/brm/platform/species/PlatformSpecies.java
+++ platform-species/src/main/java/brm/platform/species/PlatformSpecies.java
@@ -1,13 +1,17 @@
 package brm.platform.species;
+import brm.platform.architecture.loadable.IPlatformLoading;
+import brm.platform.architecture.loadable.progress.ProgressBar;
+import java.io.File;
 
 
 /**
  * <h2>PlatformSpecies</h2>
+ * The platform species handler.
  * @author Gregory
  * @see #instance instance
  * @see #PlatformSpecies PlatformSpecies
  */
-public class PlatformSpecies { //implements IPlatformLoading
+public class PlatformSpecies extends IPlatformLoading {
   /**
    * The default {@link PlatformSpecies} instance.
    */
@@ -18,5 +22,40 @@ public class PlatformSpecies { //implements IPlatformLoading
   }
 
   private PlatformSpecies() {
+  }
+
+  @Override
+  public boolean isDataLoaded() {
+    return dataLoaded;
+  }
+
+  @Override
+  public boolean isDataValidated() {
+    return dataValidated;
+  }
+
+  @Override
+  public int getInitializedCount() {
+    return initializedCount;
+  }
+
+  @Override
+  public void beforeInitialization(File f) {
+  }
+
+  @Override
+  public void initializeBefore(ProgressBar pb) {
+  }
+
+  @Override
+  public void initializeDuring(ProgressBar pb) {
+  }
+
+  @Override
+  public void initializeFinish(ProgressBar pb) {
+  }
+
+  @Override
+  public void validation() {
   }
 }
