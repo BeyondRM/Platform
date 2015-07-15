69c7e8ad4c2376ce94f12990082dfba4d4e6d6cc
JustGregory <gregory.cheyney@gmail.com>
2015-07-15 06:51:57 -0500

    Javadoc
    A bit more descriptive doc that describes the intent of a container
    class.

--- platform-window-controls/src/main/java/brm/platform/window/controls/container/AControlContainer.java
+++ platform-window-controls/src/main/java/brm/platform/window/controls/container/AControlContainer.java
@@ -3,7 +3,11 @@ import brm.platform.window.controls.AControl;
 
 
 /**
- * <h2>AControlContainer</h2>
+ * A container object. This is the abstract parent class for defining types of visual and non-visual container, used to
+ * organize multiple sub-components.
+ * <h1>Subtypes of Containers</h1>
+ * Generally, a container would be a literal frame or panel that does nothing more than hold components in a horizontal
+ * or vertical arrangement.
  * @author Gregory
  */
 abstract public class AControlContainer extends AControl {
