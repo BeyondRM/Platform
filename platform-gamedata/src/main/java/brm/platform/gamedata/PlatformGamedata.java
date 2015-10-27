d5a6da212e9484b8cc417d47239a8569abdf4be6
JustGregory <gregory.cheyney@gmail.com>
2015-10-26 21:51:25 -0500

    Added platform-gamedata.
    as above

---
+++ platform-gamedata/src/main/java/brm/platform/gamedata/PlatformGamedata.java
@@ -0,0 +1,23 @@
+package brm.platform.gamedata;
+
+
+/**
+ * The platform static data.
+ * @author Gregory <gregory.cheyney@gmail.com>
+ * @see #instance instance
+ * @see #PlatformGamedata() PlatformGamedata()
+ */
+public class PlatformGamedata {
+  /**
+   * The default {@link PlatformGamedata} instance.
+   * @see #PlatformGamedata
+   */
+  public static final PlatformGamedata instance;
+
+  static {
+    instance = new PlatformGamedata();
+  }
+
+  private PlatformGamedata() {
+  }
+}
